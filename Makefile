44fe55e6bd9065833d992307641cea7dda8d82ec
Emine Sena" Önen <eonen@k1m40s02.42istanbul.com.tr>
2025-07-14 13:32:48 +0300

    dosyalar yüklendi

---
+++ Makefile
@@ -0,0 +1,26 @@
+NAME = libftprintf.a
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+AR = ar rcs
+
+SRC = ft_printf.c
+
+OBJ = $(SRC:.c=.o)
+
+all : $(NAME)
+
+$(NAME) : $(OBJ)
+	$(AR) $(NAME) $(OBJ)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -rf $(OBJ)
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ ft_printf_func.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_func.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eonen <eonen@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 13:21:41 by eonen             #+#    #+#             */
+/*   Updated: 2025/07/14 13:25:54 by eonen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	numlen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		len = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+
+	len = numlen(n);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	if (n == 0)
+		str[0] = '0';
+	else if (n < 0)
+	{
+		str[0] = '-';
+		if (n == -2147483648)
+		{
+			str[--len] = '8';
+			n /= 10;
+		}
+		n = -n;
+	}
+	while (n != 0)
+	{
+		str[len-- - 1] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
+void	ft_putstr(char *s)
+{
+	if (!s)
+		return ;
+	while (*s != '\0')
+	{
+		write(,s, 1);
+		s++;
+	}
+}
